2119c5535939973bbaa8685ac5bc063f1d77d31b
KoGyeongtae <kokt0203@naver.com>
2021-08-31 22:47:31 +0900

    ADD - example

--- main.go
+++ main.go
@@ -1,13 +1,51 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
+
+	"github.com/Backend-Grpc-server/data"
+	userpb "github.com/Backend-Grpc-server/protos/user"
 )
 
-const portNumber = "5000"
+const portNumber = "9000"
+
+type userServer struct {
+	userpb.UserServer
+}
+
+// GetUser returns user message by user_id
+func (s *userServer) GetUser(ctx context.Context, req *userpb.GetUserRequest) (*userpb.GetUserResponse, error) {
+	userID := req.UserId
+
+	var userMessage *userpb.UserMessage
+	for _, u := range data.UserData {
+		if u.UserId != userID {
+			continue
+		}
+		userMessage = u
+		break
+	}
+
+	return &userpb.GetUserResponse{
+		UserMessage: userMessage,
+	}, nil
+}
+
+// ListUsers returns all user messages
+func (s *userServer) ListUsers(ctx context.Context, req *userpb.ListUsersRequest) (*userpb.ListUsersResponse, error) {
+	userMessages := make([]*userpb.UserMessage, len(data.UserData))
+	for i, u := range data.UserData {
+		userMessages[i] = u
+	}
+
+	return &userpb.ListUsersResponse{
+		UserMessages: userMessages,
+	}, nil
+}
 
 func main() {
 	lis, err := net.Listen("tcp", ":"+portNumber)
@@ -16,6 +54,7 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
+	userpb.RegisterUserServer(grpcServer, &userServer{})
 
 	log.Printf("start gRPC server on %s port", portNumber)
 	if err := grpcServer.Serve(lis); err != nil {
